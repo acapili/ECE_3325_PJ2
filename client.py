a3734f825a373193149482e2a6f6266d4f423f3d
Ayrthon Capili <maxthebooknerd@gmail.com>
2024-11-11 17:29:45 -0600

    Added files and comments

---
+++ client.py
@@ -0,0 +1,7 @@
+#will connect to the server on TCP port 23456
+
+#prompt for keyboard input.
+
+#“QUIT”, the client program will close the TCP connection to the server and it will then exit normally through the bottom of the program (no break or immediate quit).
+
+#“TIME” in the client program, the command will be sent to the server and the server will return its system time, then the client will display the servers system time.
---
+++ server.py
@@ -0,0 +1,4 @@
+#listen on TCP port 23456 for TCP connections.
+
+
+#determine if it is a valid command, and return the results back to the sender.  
