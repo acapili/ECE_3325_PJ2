ee662a7063d80e2966ef51dc647920da52cc7893
Ayrthon Capili <maxthebooknerd@gmail.com>
2024-11-30 17:10:50 -0600

    Finished Project

--- client.py
+++ client.py
@@ -2,6 +2,37 @@
 
 #prompt for keyboard input.
 
-#“QUIT”, the client program will close the TCP connection to the server and it will then exit normally through the bottom of the program (no break or immediate quit).
+#“QUIT”, the client program will close the TCP connection
+# to the server and it will then exit normally through the
+# bottom of the program (no break or immediate quit).
 
-#“TIME” in the client program, the command will be sent to the server and the server will return its system time, then the client will display the servers system time.
+#“TIME” in the client program, the command will be sent to the server
+# and the server will return its system time, then the client will display
+# the servers system time.
+
+from socket import *
+
+serverName = '127.0.0.1'
+serverPort = 23456
+
+
+
+clientSocket = socket(AF_INET, SOCK_STREAM)#create TCP socket for server
+clientSocket.settimeout(10) #gives the program time to connect
+clientSocket.connect((serverName, serverPort))
+
+while True:
+    sentence = input('Input TIME or QUIT cmd: ')
+
+    if sentence.lower() == 'quit':#if quit break loop
+        break
+    
+    # Send the message
+    clientSocket.send(sentence.encode())#no need to attach server name, port
+    
+    # Receive the modified message
+    modifiedSentence = clientSocket.recv(1024).decode()
+    print('From Server:', modifiedSentence)
+
+print('Closing connection to ')
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,4 +1,52 @@
 #listen on TCP port 23456 for TCP connections.
 
 
-#determine if it is a valid command, and return the results back to the sender.  
+#determine if it is a valid command, 
+#and return the results back to the sender.
+
+from socket import *
+from datetime import datetime
+
+serverPort = 23456
+serverSocket = socket(AF_INET, SOCK_STREAM)#create TCP welcoming socket
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)#server begins listening for incoming TCP requests
+
+print("Server is ready to receive")
+
+try:#closes the server from Ctrl + C
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()#Waits on accept() for incoming requests
+        print(f"Connection from {addr}")
+
+        while True:
+            sentance = connectionSocket.recv(1024).decode()#read bytes from socket
+            
+            # Case statement to handle different commands
+            match sentance.upper():
+                case "TIME":#IME cmd
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    response = f"Current server time is: {current_time}"
+                    connectionSocket.send(response.encode())
+                
+                case "QUIT":#QUIT cmd
+                    response = "Closing connection"
+                    connectionSocket.send(response.encode())
+                    break
+                
+                case _:#default for an invalid command
+                    response = "Invalid command"
+                    connectionSocket.send(response.encode())
+            
+            print(f"Sent: {response}")
+    
+        connectionSocket.close()#closes connection and goes back to waiting
+        print("Connection closed")
+
+except KeyboardInterrupt:
+    print("\nServer is shutting down")
+
+finally:
+    # Ensure the socket is closed
+    serverSocket.close()
